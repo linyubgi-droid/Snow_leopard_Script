2ce25e579a244d635b4861298d2fff6d9ea784d2
linyubgi-droid <linyubgi@gmail.com>
2026-03-26 10:21:08 +0800

    Add files via upload

---
+++ 09_treemix/step2.tree.residual.sh
@@ -0,0 +1,2 @@
+/EClab/Software/conda/Miniconda/envs/treemix/bin/Rscript ttt.R /EClab/Project/Snow_leopard/Future/06.Know_new/05.treemix/V69/00.LD/01.treemix treemix.m  poporder.txt
+#treemix.m11.treeout.gz format
